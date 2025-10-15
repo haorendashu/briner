731ec732452dfa7666a8f72a3a3f1f9aa8897ff5
DASHU <385321165@qq.com>
2025-10-15 23:04:00 +0800

    background use nostrmessageservice to handle nostr message

--- background/background.ts
+++ background/background.ts
@@ -1,12 +1,15 @@
 // import { getSerialPort } from 'js_nesigner_sdk'
 import type { ISigner } from '../business/nostr_signer/isigner';
 import { NsecSigner } from '../business/nostr_signer/nsec_signer';
+import { NostrMessageService } from '../business/service/nostr_message_service';
 
 console.log('Hello from the background script!')
 
-let signer: ISigner | null;
+let nostrMessageService = new NostrMessageService();
 
-signer = new NsecSigner('');
+let signer = new NsecSigner('');
+var pubkey = await signer.getPublicKey();
+nostrMessageService.addSigner(pubkey, signer);
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     // message: 发送的消息内容
@@ -15,64 +18,29 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log("receive message:", message)
     console.log("sender:", sender)
 
-    let origin = sender.origin
-    let url = sender.url
-
-    let id = message.id
-    let type = message.type
-    let params = message.params
-
-    switch (type) {
-        case 'getPublicKey':
-            {
-                signer?.getPublicKey().then((res) => {
-                    console.log('getPublicKey:', res)
-                    sendResponse({ id: id, response: res })
-                })
-                break;
-            }
-        case 'signEvent':
-            signer?.signEvent(params).then((res) => {
-                sendResponse({ id: id, response: res })
-            })
-            break;
-        case 'nip04Decrypt':
-            {
-                let pubkey = params.pubkey
-                let text = params.text
-                signer?.nip04Decrypt(pubkey, text).then((res) => {
-                    sendResponse({ id: id, response: res })
-                })
-                break;
-            }
-        case 'nip04Encrypt':
-            {
-                let pubkey = params.pubkey
-                let text = params.text
-                signer?.nip04Encrypt(pubkey, text).then((res) => {
-                    sendResponse({ id: id, response: res })
-                })
-                break;
-            }
-        case 'nip44Decrypt':
-            {
-                let pubkey = params.pubkey
-                let text = params.text
-                signer?.nip44Decrypt(pubkey, text).then((res) => {
-                    sendResponse({ id: id, response: res })
-                })
-                break;
-            }
-        case 'nip44Encrypt':
-            {
-                let pubkey = params.pubkey
-                let text = params.text
-                signer?.nip44Encrypt(pubkey, text).then((res) => {
-                    sendResponse({ id: id, response: res })
-                })
-                break;
-            }
+    if (nostrMessageService.handle(message, sender, sendResponse)) {
+        return true;
     }
+
+    // TODO handle other message type
+
+    // let origin = sender.origin
+    // let url = sender.url
+
+    // let id = message.id
+    // let type = message.type
+    // let params = message.params
+
+    // switch (type) {
+    //     case 'getPublicKey':
+    //         {
+    //             signer?.getPublicKey().then((res) => {
+    //                 console.log('getPublicKey:', res)
+    //                 sendResponse({ id: id, response: res })
+    //             })
+    //             break;
+    //         }
+    // }
 
     // sendResponse({ id: id, response: "Hello from background script!" });
     // chrome.action.openPopup()
