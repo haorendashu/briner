983bf693cd04dae1e6d9fb1396be7c50df7bce5c
DASHU <385321165@qq.com>
2025-11-05 01:04:58 +0800

    change app bar component ui

--- ui/components/app_bar_component.vue
+++ ui/components/app_bar_component.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex items-center justify-between container bg-white pt-2 pb-2 pl-3 pr-3">
+    <div class="flex items-center justify-between container bg-white p-3 rounded-br-2xl rounded-bl-2xl">
         <div class="flex items-center">
             <a v-if="showBack == 'true'" @click="$router.go(-1)" href="javascript:void(0)" class="mr-4 px-2 font-bold text-xl"><</a>
             <h1 class="text-xl">{{ title }}</h1>
