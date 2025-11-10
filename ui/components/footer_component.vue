17511ba1baaafb9ad598d7be3ca1e71ecef70543
DASHU <385321165@qq.com>
2025-11-10 23:22:56 +0800

    change footer's text

--- ui/components/footer_component.vue
+++ ui/components/footer_component.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="py-4 border-t">
       <div class="container mx-auto text-center text-gray-600">
-        © {{ new Date().getFullYear() }} 版权所有
+        Support by <a href="https://github.com/haorendashu/briner/issues" target="_blank" class="text-blue-500">Briner</a>
       </div>
     </footer>
 </template>
