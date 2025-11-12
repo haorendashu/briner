ab2f4714e7d1d56fa5d904f8702f61b07692317b
DASHU <385321165@qq.com>
2025-11-12 18:54:12 +0800

    when user haven't add show add user UI

--- ui/index/index.vue
+++ ui/index/index.vue
@@ -81,58 +81,69 @@ onMounted(async () => {
 </script>
 
 <template>
-    <div v-if="users.length > 0" class="container">
-        <div class="flex pl-4 pr-4 pt-4 items-center" v-on:click="toUsersPage">
-            <CircleImageComponent
-                imageUrl="/imgs/user_white.png"
-                :diameter="50"
-                altText="用户头像"
-                class="ml-4"
-                backgroundColor="#333333"
-            />
-            <div class="ml-4 text-lg">
-                <UsernameComponent :pubkey="users[0].pubkey" />
-            </div>
-        </div>
-        <div v-if="users.length > 1" class="card mt-4" v-on:click="toUsersPage">
-            <div class="flex items-center pl-4 pr-4">
-                <div v-for="user in users" class="mr-2 flex items-center">
-                    <CircleImageComponent 
-                        imageUrl="/imgs/user_white.png"
-                        :diameter="30"
-                        altText="用户头像"
-                        backgroundColor="#333333"
-                    />
-                </div>
-                <div class="ml-auto">
-                    <p class="text-lg">&gt;</p>
+    <template v-if="users.length <= 0">
+        <div class="container">
+            <div class="card mt-8">
+                <div class="p-8 text-xl text-center">
+                    <p>You haven't added any users yet. <a v-on:click="toAddUser" class="font-bold">Let's start.</a></p>
                 </div>
             </div>
         </div>
-    </div>
-    <div v-if="users.length <= 0" class="container mt-4 pl-4">
-        <div class="text-left">
-            <p class="text-lg font-bold ml-4" v-on:click="toAddUser">Click and Login</p>
-        </div>
-    </div>
-
-    <div class="container">
-        <div class="card mt-4">
-            <AppItemComponent v-for="app in apps" :app="app" />
-
-            <div class="item justify-center">
-                <RouterLink to="/apps" class="mr-4">Show more apps</RouterLink>
+    </template>
+    <template v-else>
+        <div v-if="users.length > 0" class="container">
+            <div class="flex pl-4 pr-4 pt-4 items-center" v-on:click="toUsersPage">
+                <CircleImageComponent
+                    imageUrl="/imgs/user_white.png"
+                    :diameter="50"
+                    altText="用户头像"
+                    class="ml-4"
+                    backgroundColor="#333333"
+                />
+                <div class="ml-4 text-lg">
+                    <UsernameComponent :pubkey="users[0].pubkey" />
+                </div>
+            </div>
+            <div v-if="users.length > 1" class="card mt-4" v-on:click="toUsersPage">
+                <div class="flex items-center pl-4 pr-4">
+                    <div v-for="user in users" class="mr-2 flex items-center">
+                        <CircleImageComponent 
+                            imageUrl="/imgs/user_white.png"
+                            :diameter="30"
+                            altText="用户头像"
+                            backgroundColor="#333333"
+                        />
+                    </div>
+                    <div class="ml-auto">
+                        <p class="text-lg">&gt;</p>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-
-    <div class="container">
-        <div class="card mt-4">
-            <AuthLogItemComponent v-for="authLog in authLogs" :authLog="authLog" />
-
-            <div class="item justify-center">
-                <RouterLink to="/logs" class="mr-4">Show more logs</RouterLink>
+        <div v-if="users.length <= 0" class="container mt-4 pl-4">
+            <div class="text-left">
+                <p class="text-lg font-bold ml-4" v-on:click="toAddUser">Click and Login</p>
             </div>
         </div>
-    </div>
+
+        <div class="container">
+            <div class="card mt-4">
+                <AppItemComponent v-for="app in apps" :app="app" />
+
+                <div class="item justify-center">
+                    <RouterLink to="/apps" class="mr-4">Show more apps</RouterLink>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="card mt-4">
+                <AuthLogItemComponent v-for="authLog in authLogs" :authLog="authLog" />
+
+                <div class="item justify-center">
+                    <RouterLink to="/logs" class="mr-4">Show more logs</RouterLink>
+                </div>
+            </div>
+        </div>
+    </template>
 </template>
