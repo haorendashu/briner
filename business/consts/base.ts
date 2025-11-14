e80f30489bea7823879a2347a581c6cc9ace8ac0
DASHU <385321165@qq.com>
2025-11-14 12:50:31 +0800

    change foot component add version

---
+++ business/consts/base.ts
@@ -0,0 +1,6 @@
+
+export const Base = {
+    Name: "Briner",
+    Version: "0.0.1",
+    SupportUrl: "https://github.com/haorendashu/briner/issues",
+}
--- ui/components/footer_component.vue
+++ ui/components/footer_component.vue
@@ -1,12 +1,13 @@
 <template>
     <footer class="py-4 border-t">
       <div class="container mx-auto text-center text-gray-600">
-        Support by <a href="https://github.com/haorendashu/briner/issues" target="_blank" class="text-blue-500">Briner</a>
+        Support by <a :href="Base.SupportUrl" target="_blank" class="text-blue-500">{{ Base.Name }}</a> v{{ Base.Version }}
       </div>
     </footer>
 </template>
 
 <script setup lang="ts">
+import { Base } from "../../business/consts/base";
 </script>
 
 <style scoped>
